b6fc0812a1f3e6aa277253df150557a263771b79
Terron Johnson <tmj72386@gmail.com>
2017-09-18 05:51:23 -0700

    ServerAttached

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
-'use strict'
-//first we import our dependencies…
-var express = require("express");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 //and create our instances
-var app = express();
-var router = express.Router();
+const app = express();
+const router = express.Router();
 //set our port to either a predetermined port number if you have set 
 //it up, or 3001
-var port = process.env.API_PORT || 3001;
+const port = process.env.API_PORT || 3001;
+const Comment = require('./model/comment');
+
+
+//db config
+mongoose.connect("mongodb://tmj:test123@ds141534.mlab.com:41534/commentbox2");
+
+
+//now we should configure the API to use bodyParser and look for 
+//JSON data in the request body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//To prevent errors from Cross Origin Resource Sharing, we will set 
+//our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+ res.setHeader("Access-Control-Allow-Origin", "*");
+ res.setHeader("Access-Control-Allow-Credentials", "true");
+ res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+ res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+//and remove cacheing so we get the most recent comments
+ res.setHeader("Cache-Control", "no-cache");
+ next();
+});
+//now we can set the route path & initialize the API
+router.get("/", function(req, res) {
+ res.json({ message: "API Initialized!"});
+});
+//Use our router configuration when we call /api
+app.use("/api", router);
+//starts the server and listens for requests
+app.listen(port, function() {
+ console.log(`api running on port ${port}`);
+});
